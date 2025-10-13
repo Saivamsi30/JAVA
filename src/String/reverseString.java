75effcea5e14f5a167acb21b37b5ef04ad7a06bf
Saivamsi30 <saivamsi8239@gmail.com>
2025-10-13 15:19:50 +0530

    Reverse String

--- src/String/reverseString.java
+++ src/String/reverseString.java
@@ -4,13 +4,11 @@ public class reverseString {
     public static void main(String[] args) {
         String s1 = "abhishek";
 
-        for(int i= 0;i<s1.length();i++)
-        {
+        for (int i = 0; i < s1.length(); i++) {
             System.out.print(s1.charAt(i));
         }
         System.out.println();
-        for(int i = s1.length() - 1;i>=0;i--)
-        {
+        for (int i = s1.length() - 1; i >= 0; i--) {
             System.out.print(s1.charAt(i));
         }
 
