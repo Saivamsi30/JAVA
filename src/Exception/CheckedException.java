9b34f10a687077af0b6385beb52006d5da2467e9
Saivamsi30 <saivamsi8239@gmail.com>
2025-10-22 12:02:00 +0530

    Checked Exception

---
+++ src/Exception/CheckedException.java
@@ -0,0 +1,11 @@
+package Exception;
+
+import java.io.FileReader;
+
+public class CheckedException {
+    public static void main(String[] args) {
+        FileReader file = new FileReader("Sai Vamsi");
+
+    }
+}
+//FileNotFoundException
