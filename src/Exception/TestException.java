829fea1a7bf3e2179331c37dcd8d7ca50b33fac2
Saivamsi30 <saivamsi8239@gmail.com>
2025-10-22 11:31:45 +0530

    Test Exception

---
+++ src/Exception/TestException.java
@@ -0,0 +1,13 @@
+package Exception;
+
+public class TestException {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Sai Vamsi");
+        System.out.println(10/0);
+        System.out.println("How are you?");
+        System.out.println("How is it going?");
+//    Runtime Exception----> crashed at Runtime with Arithmetic expression
+    }
+
+}
