05c7200b84cf7d3c92868827ae7f893478d631d8
Saivamsi30 <saivamsi8239@gmail.com>
2025-10-25 12:03:31 +0530

    Q1

---
+++ src/Questions/Q1.java
@@ -0,0 +1,15 @@
+package Questions;
+
+public class Q1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        try{
+            System.out.println("I am");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception");
+        }
+        System.out.println("Done");
+    }
+}
